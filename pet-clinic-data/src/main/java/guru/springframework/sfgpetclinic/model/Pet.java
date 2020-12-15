71ac1df81c3578ba0e7ee235bf8e43e2f37f4650
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2020-12-15 10:59:48 +0200

    Enhanced owners with pets and contact info. Closes #33.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -6,6 +6,15 @@ public class Pet extends BaseEntity {
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public void setPetType(PetType petType) {
         this.petType = petType;
